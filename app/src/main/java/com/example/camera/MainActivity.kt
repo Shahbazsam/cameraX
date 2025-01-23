69484efa7f28ef1da8532b0e8c5396b42fbdacd0
Mohammad Shahbaz <mdshahbaz7896@gmail.com>
2025-01-23 19:21:11 +0700

    update

--- app/src/main/java/com/example/camera/MainActivity.kt
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -4,14 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.camera.compose.CameraXViewfinder
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.camera.ui.CameraPreviewViewModel
 import com.example.camera.ui.theme.CameraTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +39,74 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CameraTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val viewModel = remember { CameraPreviewViewModel() }
+                CameraPreviewScreen(viewModel)
             }
         }
     }
 }
 
+
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CameraPreviewScreen(
+    viewModel: CameraPreviewViewModel,
+    modifier: Modifier = Modifier
+) {
+    val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+
+    if (cameraPermissionState.status.isGranted) {
+        CameraPreviewContent(viewModel)
+    } else {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .wrapContentSize()
+                .widthIn(max = 480.dp),
+        ) {
+            val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
+                "Whoops! Looks like we need your camera to work our magic!" +
+                        "Don't worry, we just wanna see your pretty face (and maybe some cats).  " +
+                        "Grant us permission and let's get this party started!"
+            } else {
+                "Hi there! We need your camera to work our magic! ✨\n" +
+                        "Grant us permission and let's get this party started! \uD83C\uDF89"
+            }
+            Text(
+                text = textToShow,
+                textAlign = TextAlign.Center
+            )
+            Spacer(Modifier.height(16.dp))
+            Button(
+                onClick = {cameraPermissionState.launchPermissionRequest()}
+            ) {
+                Text(
+                    text = "Grant Permission"
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    CameraTheme {
-        Greeting("Android")
+private fun CameraPreviewContent(
+    viewModel: CameraPreviewViewModel,
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    modifier: Modifier = Modifier
+    ) {
+    val surfaceRequest = viewModel.surfaceRequest.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+    LaunchedEffect(lifecycleOwner) {
+        viewModel.bindToCamera(context , lifecycleOwner)
     }
+
+    surfaceRequest.value?.let { request ->
+        CameraXViewfinder(
+            surfaceRequest = request,
+            modifier = modifier
+        )
+
+    }
+
 }
